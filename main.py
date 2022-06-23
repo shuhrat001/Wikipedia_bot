6d68c8a5b44be06505ad0701d9b7070142d20d16
shuhrat001 <107908893+shuhrat001@users.noreply.github.com>
2022-06-23 09:37:51 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import logging
+import wikipedia
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '5580243715:AAG-sqmUMFd_0tCkX_gqLrdS5xzPQiIHa8A'
+wikipedia.set_lang('uz')
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Wikipedia botiga xush kelibsiz!")
+
+@dp.message_handler()
+async def sendWiki(message: types.Message):
+    try:
+        respond = wikipedia.summary(message.text)
+        await message.answer(respond)
+    except:
+        await message.answer("Bu mavzuga oid maqola topilmadi")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
